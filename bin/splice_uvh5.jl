60ae31ba656299240747f877777e8a4143987b29
David MacMahon <davidm@astro.berkeley.edu>
2022-02-14 10:52:16 +0200

    Make bin/splice_uvh5.jl script more usable

--- bin/splice_uvh5.jl
+++ bin/splice_uvh5.jl
@@ -1,2 +1,13 @@
-using UVH5Splicer
-uvh5_splice(ARGS...)
+#!/bin/bash
+#=
+export JULIA_PROJECT=$(dirname $(dirname $(readlink -e "${BASH_SOURCE[0]}")))
+exec julia --color=yes --startup-file=no "${BASH_SOURCE[0]}" "$@"
+=#
+
+if length(ARGS) < 2
+    error("usage: $(PROGRAM_FILE) OUTFILE INFILE1 [INFILE2 [...]]")
+else
+    using UVH5Splicer
+    outfile = popfirst!(ARGS)
+    uvh5_splice(outfile, ARGS)
+end
